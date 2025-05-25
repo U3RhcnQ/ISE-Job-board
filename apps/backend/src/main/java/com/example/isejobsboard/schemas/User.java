30b043f28513ad5800ee71811cf136957e8be94a
Cian <cian_mc_namara@hotmail.com>
2025-05-25 20:35:28 +0100

    [schemas/User.java] Updated User record with accurate information

--- apps/backend/src/main/java/com/example/isejobsboard/schemas/User.java
+++ apps/backend/src/main/java/com/example/isejobsboard/schemas/User.java
@@ -1,9 +1,12 @@
 package com.example.isejobsboard.schemas;
 
-public record User(String forename, String surname, int studentId) {
-    public User(String forename, String surname, int studentId) {
+public record User(int user_id, String email, String forename, String surname, int user_level, String organisation) {
+    public User(int user_id, String email, String forename, String surname, int user_level, String organisation) {
+        this.user_id = user_id;
+        this.email = email;
         this.forename = forename;
         this.surname = surname;
-        this.studentId = studentId;
+        this.user_level = user_level;
+        this.organisation = organisation;
     }
 }
