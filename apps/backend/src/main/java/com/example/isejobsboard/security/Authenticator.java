a0b500510a00ff74edfa4671f56e544f9924c2d8
Cian <cian_mc_namara@hotmail.com>
2025-05-26 13:36:25 +0100

    [security/Authenticator.java] Created SQL connection to db

--- apps/backend/src/main/java/com/example/isejobsboard/security/Authenticator.java
+++ apps/backend/src/main/java/com/example/isejobsboard/security/Authenticator.java
@@ -1,9 +1,15 @@
 package com.example.isejobsboard.security;
 
 import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Base64;
 
 public class Authenticator {
+    public Authenticator() {}
+
     public void createToken() {
         String token = _buildToken();
 
@@ -15,7 +21,14 @@ public class Authenticator {
     }
 
     public boolean isTokenValid(String token) {
+        Connection tokensConnection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/login_sessions",
+                "root",
+                ""
+        );
 
+        Statement tokensStatement = tokensConnection.createStatement();
+        ResultSet tokensResultSet = tokensStatement.executeQuery("SELECT * FROM login_sessions");
     }
 
     /**
