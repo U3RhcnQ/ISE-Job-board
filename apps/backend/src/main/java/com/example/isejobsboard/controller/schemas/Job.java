75045b497ee26630b9e56633df2932cfa69ca5e4
patshyo <patricksyms8@gmail.com>
2025-05-29 10:51:37 +0100

    insertJob method created

--- apps/backend/src/main/java/com/example/isejobsboard/controller/schemas/Job.java
+++ apps/backend/src/main/java/com/example/isejobsboard/controller/schemas/Job.java
@@ -1,67 +1,120 @@
 package com.example.isejobsboard.controller.schemas;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Map;
+
+
+
+enum Approval{
+    APPROVED,
+    PENDING,
+    REJECTED
+}
 public class Job {
-    private Long jobId;
-    private Long companyId;
-    private int positionCount;
-    private String description;
-    private String jobTitle;
-    private int residencyId;
-    private String Salary;
+    private final Long jobId;
+    private  final Long companyId;
+    private final int positionCount;
+    private final String description;
+    private final String smallDes;
+    private final String residency;
+    private final String jobTitle;
+    private final Long addressId;
+    private final String residencyTitle;
+    private final Approval approval;
+    private final String salary;
+    private static final Map<String, String> env = System.getenv();
+
+    public Job(Long jobId, Long companyId,
+               int positionCount, String description,
+               String smallDes, String residency,
+               String jobTitle, Long addressId,
+               String residencyTitle, Approval approval,
+               String salary) {
+        this.jobId = jobId;
+        this.companyId = companyId;
+        this.positionCount = positionCount;
+        this.description = description;
+        this.smallDes = smallDes;
+        this.residency = residency;
+        this.jobTitle = jobTitle;
+        this.addressId = addressId;
+        this.residencyTitle = residencyTitle;
+        this.approval = approval;
+        this.salary = salary;
+    }
 
     public Long getJobId() {
         return jobId;
-    }
-
-    public void setJobId(Long jobId) {
-        this.jobId = jobId;
     }
 
     public Long getCompanyId() {
         return companyId;
     }
 
-    public void setCompanyId(Long companyId) {
-        this.companyId = companyId;
-    }
-
     public int getPositionCount() {
         return positionCount;
-    }
-
-    public void setPositionCount(int positionCount) {
-        this.positionCount = positionCount;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getSmallDes() {
+        return smallDes;
+    }
+
+    public String getResidency() {
+        return residency;
     }
 
     public String getJobTitle() {
         return jobTitle;
     }
 
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
+    public Long getAddressId() {
+        return addressId;
     }
 
-    public int getResidencyId() {
-        return residencyId;
+    public String getResidencyTitle() {
+        return residencyTitle;
     }
 
-    public void setResidencyId(int residencyId) {
-        this.residencyId = residencyId;
+    public Approval getApproval() {
+        return approval;
+    }
+    public String getSalary(){
+        return this.salary;
     }
 
-    public String getSalary() {
-        return Salary;
-    }
+    public static void insertNewJob(Job job){
+        String sql = "INSERT INTO job ("+
+                "company_id, position_count"+
+                "description, job_title, salary,"+
+                "small_description, residency, "+
+                "residency_title, address_id)"+
+                "VALUES (?,?,?,?,?,?,?,?,?)";
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://isejobsboard.petr.ie:3306/jobs_board",
+                env.get("MYSQL_USER_NAME"), env.get("MYSQL_USER_PASSWORD"));
+             PreparedStatement statement = connection.prepareStatement(sql)) {
 
-    public void setSalary(String salary) {
-        Salary = salary;
+            statement.setLong(1,job.getCompanyId());
+            statement.setInt(2,job.getPositionCount());
+            statement.setString(3,job.getDescription());
+            statement.setString(4,job.getJobTitle());
+            statement.setString(5,job.getSalary());
+            statement.setString(6,job.getSmallDes());
+            statement.setString(7,job.getResidency());
+            statement.setString(8,job.getResidencyTitle());
+            statement.setLong(9,job.getAddressId());
+
+            statement.executeUpdate();
+
+        }catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
