a53ba3bcbd957a3ffcf8751c8922c67de68b678f
U3RhcnQ <165941707+U3RhcnQ@users.noreply.github.com>
2025-06-06 00:06:16 +0100

    Testing

--- apps/backend/src/main/java/com/example/isejobsboard/Utils/DatabaseUtils.java
+++ apps/backend/src/main/java/com/example/isejobsboard/Utils/DatabaseUtils.java
@@ -1,8 +1,19 @@
 package com.example.isejobsboard.Utils;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class DatabaseUtils {
-    public static String url = "jdbc:mysql://isejobsboard.petr.ie:3306/jobs_board?allowMultiQueries=true";
+
+    public static String url;
     public static final Map<String, String> env = System.getenv();
+
+    // Static initializer block: This code runs once when the class is loaded
+    static {
+        // Attempt to get the database URL from the "DATABASE_URL" environment variable.
+        url = Optional.ofNullable(System.getenv("DATABASE_URL"))
+                .orElse("jdbc:mysql://localhost:3306/jobs_board?allowMultiQueries=true");
+
+    }
+
 }
